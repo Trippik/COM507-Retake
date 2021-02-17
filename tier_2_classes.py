98ac8696baaea35db6f6ed5ba4260a9ab75296cf
Trippik <43576788+Trippik@users.noreply.github.com>
2021-02-17 00:12:09 +0000

    Update tier_2_classes.py

--- tier_2_classes.py
+++ tier_2_classes.py
@@ -33,23 +33,34 @@ class Spaceship(base_classes.Agent):
         self.inventory = []
         super().__init__(ship_position[0], ship_position[1])
     
-    def scan(self, target):
+    def scan(self, targets):
         agents = self.environment.get_agents()
-        for agent in agents:
-            if((target == agent.getter()) and (type(agent).__name__) == Rover):
-                if(agent.inventory != None):
-                    self.inventory = self.inventory + [agent.inventory, ]
+        for target in targets:
+            for agent in agents:
+                if((target == agent.getter()) and (type(agent).__name__) == Rover):
+                    if(agent.inventory != None):
+                        self.inventory = self.inventory + [agent.inventory, ]
                 agent.battery_level = 100
 
     def scan_charge(self):
         ship_loc = self.getter()
         x = ship_loc[0]
         y = ship_loc[1]
-        x_target = x - 1
+        targets = []
+        x_target_m = x - 1
+        y_target_m = x - 1
+        targets = targets + [(x, y_target_m), (x_target_m, y), (x_target_m, y_target_m),]
+        x_target_p = x + 1
+        y_target_p = x + 1
+        targets = targets + [(x, y_target_p), (x_target_p, y), (x_target_p, y_target_p),]
+        targets = targets + [(x_target_m, y_target_p), (x_target_p, y_target_m)]
+        self.scan(targets)
+
+
 
 #-----TESTING OF MARS CLASS-----
-#test_mars = Mars()
-#test_mars.set_size(5,5)
+test_mars = Mars()
+test_mars.set_size(5,5)
 #print(test_mars.get_size())
 
 #-----TESTING OF ROVER CLASS-----
@@ -58,3 +69,16 @@ class Spaceship(base_classes.Agent):
 #print(test_rover.getter())
 #print(test_rover.battery_level)
 #print(type(test_rover).__name__)
+
+#-----TESTING OF SPACESHIP CLASS-----
+#test_ship = Spaceship([0,0])
+#test_ship.set_environment(test_mars)
+#test_rover = Rover([1,1], test_ship.getter(), 20)
+#test_rover.set_environment(test_mars)
+#print(test_rover.battery_level)
+#test_ship.scan_charge()
+#print(test_rover.battery_level)
+#test_rover.move([-1,0], 2)
+#print(test_rover.battery_level)
+#test_ship.scan_charge()
+#print(test_rover.battery_level)
