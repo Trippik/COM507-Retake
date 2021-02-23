a647dfa2c0f8664f219f1a3e6fab065a11495eec
Trippik <43576788+Trippik@users.noreply.github.com>
2021-02-23 22:22:23 +0000

    Create mars.py
    classes split out into 1 file per class as per Prins's instructions, each class/file is desgined to be as standalone as possible

---
+++ lib/mars.py
@@ -0,0 +1,11 @@
+#-----IMPORT NECESSARY CLASSES AND LIBRARIES-----
+import base_classes.environment as environment
+
+#MARS CLASS
+class Mars(environment.Environment):
+    pass
+
+#-----TESTING OF MARS CLASS-----
+#test_mars = Mars()
+#test_mars.set_size(5,5)
+#print(test_mars.get_size())
