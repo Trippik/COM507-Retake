620fe2280b964b14b8f7082bfa17211b85bf7dcc
Trippik <43576788+Trippik@users.noreply.github.com>
2021-03-03 20:47:04 +0000

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,6 +2,5 @@ from simulation import Simulation
 from Gui_Testing import gui
 import time
 
-sim = Simulation([10,10], 4, 20)
+sim = Simulation([10,10], 4, 10)
 window = gui(sim, [600,600])
-window.refresh_items()
